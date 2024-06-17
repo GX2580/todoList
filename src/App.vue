2a989200f3a3bbb0d52cf5338df8b03382545972
gexiao <xiaoge740@gmail.com>
2024-06-17 09:08:20 +0800

    init the project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="component-name">aaa</div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<style scoped>
+.component-name {
+  text-align: center;
+}
+</style>
