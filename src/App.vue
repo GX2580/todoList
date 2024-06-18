26ccfb98c88f5cfb0b207170c8f8d279c63d24d7
gexiao <xiaoge740@gmail.com>
2024-06-18 11:20:59 +0800

    use hooks to Encapsulation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <TodoHeader @add-todo="hadnleAddTodo"></TodoHeader>
+  <TodoHeader @add-todo="handleAddTodo"></TodoHeader>
   <TodoList :todos="todos">
     <TodoItem
       v-for="todo in todos"
       :key="todo.id"
       v-bind="todo"
       @toggle="handleToggle"
-      @edit="hanleEdit"
+      @edit="handleEdit"
       @del="handleDel"
     ></TodoItem>
   </TodoList>
@@ -18,55 +18,22 @@
   ></TodoFooter>
 </template>
 <script setup>
-import { ref, onMounted, watch } from 'vue'
 import TodoList from './components/TodoList.vue'
 import TodoHeader from './components/TodoHeader.vue'
 import TodoFooter from './components/TodoFooter.vue'
 import TodoItem from './components/TodoItem.vue'
+import useTodos from './hooks/useTodos.js'
 
-const todos = ref([])
-
-const hadnleAddTodo = (todo) => {
-  todos.value.push(todo)
-}
-
-const handleToggle = (id, checked) => {
-  const index = todos.value.findIndex((todo) => todo.id === id)
-  todos.value[index].done = checked
-}
-
-const hanleEdit = (id, content) => {
-  const index = todos.value.findIndex((todo) => todo.id === id)
-  todos.value[index].content = content
-}
-const handleToggleAll = (checked) => {
-  todos.value.forEach((todo) => {
-    todo.done = checked
-  })
-}
-const handleClearDone = () => {
-  todos.value = todos.value.filter((todo) => !todo.done)
-}
-const handleClearAll = () => {
-  todos.value = []
-}
-const handleDel = (id) => {
-  const index = todos.value.findIndex((todo) => todo.id === id)
-  todos.value.splice(index, 1)
-}
-watch(
+const {
   todos,
-  () => {
-    console.log('something changed')
-    localStorage.setItem('todos', JSON.stringify(todos.value))
-  },
-  { deep: true }
-)
-onMounted(() => {
-  //update data
-  const data = JSON.parse(localStorage.getItem('todos') || '[]')
-  todos.value = data
-})
+  handleAddTodo,
+  handleToggle,
+  handleEdit,
+  handleDel,
+  handleToggleAll,
+  handleClearDone,
+  handleClearAll,
+} = useTodos()
 </script>
 <style>
 * {
